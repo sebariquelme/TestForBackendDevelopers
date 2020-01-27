b47d24a0e1b1f696cb15859acf78fb92ffecc0ab
sebariquelme <searique@egresados.ubiobio.cl>
2020-01-27 15:57:57 -0300

    changes on person

--- src/main/java/com/nisum/test/entity/Person.java
+++ src/main/java/com/nisum/test/entity/Person.java
@@ -6,6 +6,8 @@ import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 import javax.persistence.*;
+import javax.validation.constraints.Pattern;
+import java.util.Date;
 import java.util.Set;
 
 @Entity
@@ -14,10 +16,42 @@ import java.util.Set;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "PERSON")
-public class Person extends BaseEntity {
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "create_at")
+    @Temporal(TemporalType.DATE)
+    private Date created;
+
+    @Column(name = "update_at")
+    @Temporal(TemporalType.DATE)
+    private Date modified;
+
+    @Column(name = "delete_at")
+    @Temporal(TemporalType.DATE)
+    private Date deleted;
+
+    @Column(name = "last_login")
+    @Temporal(TemporalType.DATE)
+    private Date lastLogin;
+
+    @Column(name = "is_active")
+    private boolean isActive;
+
+    private String token;
+
     private String name;
+
+    @Pattern(regexp = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$")
+    private String email;
+
+    @Pattern(regexp = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{6,}$")
     private String password;
+
     @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "person")
     private Set<PhoneNumber> phones;
-    private String token;
+
 }
